0dcf53508d8c6108ac12dab8a5201732f4539ab4
Kristof Overdulve <kristof.overdulve@kbc.be>
2015-01-16 08:42:52 +0100

    Getting the git commit messages for all new changes

---
+++ interpret-commit-message.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+KEY = 'Bulletin'
+
+def createBulletin(commit, bulletin)
+  printf "Commit: #{commit}\n"
+  printf "Bulletin:\n#{bulletin}"
+  printf '\n'
+end
+
+def processCommitMessage(commit, commitmessage)
+  multilineBulletin = nil
+
+  commitmessage.split("\n").each do |line|
+    if multilineBulletin != nil
+      # Continue reading until empty line reached
+      if line.strip == ""
+        # End reached
+        createBulletin commit, multilineBulletin
+        multilineBulletin = nil
+      else
+        # Continue reading multiline
+        multilineBulletin += line + "\n"
+      end
+    else
+      # Check for single and multiline bulletin
+      if line == KEY
+        # Multiline bulletin starts
+        multilineBulletin = ""
+      elsif line.index("#{KEY}:")
+        # Single line bulletin
+        singleLineBulletinIndex = line.index("#{KEY}:")
+        singlelineBulletin = line[(singleLineBulletinIndex + "#{KEY}:".length)..-1] + '\n'
+        createBulletin commit, singlelineBulletin
+      end
+    end
+  end
+
+  createBulletin commit, multilineBulletin if multilineBulletin != nil
+end
+
+processCommitMessage ARGV[0], ARGV[1]
